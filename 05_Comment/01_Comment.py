9de837379d10566051e4035a085a20eccb4e9b6f
priyanshu <priyanshuverma7245@gmail.com>
2025-04-25 10:25:29 +0530

    change

---
+++ 05_Comment/01_Comment.py
@@ -0,0 +1,24 @@
+# hello kaise ho
+'''
+hello priyanshu
+verma 
+kaise ho 
+'''
+
+# but in python there is no multiline comment exist but in actual world it is docstring that contain little idea about the functions
+
+import sys as s
+
+def pridoc():
+    '''
+    It is Printing Funtion
+    '''
+    a = 5
+
+
+print(pridoc.__doc__)
+
+#Output  It is Printing Funtion
+
+
+
